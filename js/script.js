998f688767edcca7308429069e7bc444a40d646e
Abdullah Al Masud <masudctg00@gmail.com>
2022-02-09 20:23:41 +0600

    border radius changed to 30px

--- js/script.js
+++ js/script.js
@@ -12,3 +12,13 @@ for (const h2 of h2Color) {
  ----------------------*/
 const backpackBgColor = document.getElementById('backpack');
 backpackBgColor.style.backgroundColor = 'tomato'
+
+/*----------------------
+ class='card' border 
+ radius 30px 
+ ----------------------*/
+
+const cards = document.querySelectorAll('.card');
+for (const card of cards) {
+    card.style.borderRadius = '30px'
+}
