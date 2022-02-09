e88429e5f244b52d57e5a3d93ab823245fecbaf8
Abdullah Al Masud <masudctg00@gmail.com>
2022-02-09 21:09:17 +0600

    practice done

--- js/script.js
+++ js/script.js
@@ -22,3 +22,55 @@ const cards = document.querySelectorAll('.card');
 for (const card of cards) {
     card.style.borderRadius = '30px'
 }
+
+/*----------------------
+btn click handler to
+console log
+ ----------------------*/
+
+const buyNowBtn = document.getElementsByClassName('panda-btn-buy-now');
+for (const buyBtn of buyNowBtn) {
+    buyBtn.addEventListener('click', function() {
+        buyBtn.style.display = 'none'
+            //buyBtn.parentNode.removeChild(buyBtn)
+    })
+}
+
+/*----------------------
+Submit button disable
+ ----------------------*/
+
+const submitBtn = document.getElementById('submit-btn')
+const inputConfirm = document.getElementById('input-confirm')
+inputConfirm.addEventListener('keyup', function() {
+    if (inputConfirm.value == 'email') {
+        submitBtn.removeAttribute('disabled')
+    } else {
+        submitBtn.setAttribute('disabled', true)
+    }
+})
+
+/*--------------------------
+change image on mouse enter
+ --------------------------*/
+
+const watchPic = document.getElementById('watch-Pic')
+const text = document.getElementById('text');
+watchPic.addEventListener('mouseenter', function() {
+    watchPic.src = "images/categories/bag.png"
+    text.innerHTML = "Bag"
+})
+watchPic.addEventListener('mouseleave', function() {
+    watchPic.src = "images/categories/watch.png"
+    text.innerHTML = "Watch"
+})
+
+/*--------------------------
+double click on lets stay
+in touch to change bg-color
+ --------------------------*/
+
+const contactSection = document.getElementById('contact-section');
+contactSection.addEventListener('dblclick', function() {
+    contactSection.style.backgroundColor = 'yellow'
+})
