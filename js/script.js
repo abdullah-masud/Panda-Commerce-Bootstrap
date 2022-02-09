3bba0fd8585aa8aa655b1b71debc721b165ee29c
Abdullah Al Masud <masudctg00@gmail.com>
2022-02-09 20:13:22 +0600

    id: backpack background color changed to tomato

--- js/script.js
+++ js/script.js
@@ -1,5 +1,14 @@
-// All h2 tag lightblue
+/*----------------------
+ All h2 tag lightblue
+ ----------------------*/
 const h2Color = document.getElementsByTagName('h2');
 for (const h2 of h2Color) {
     h2.style.color = 'lightblue'
 }
+
+/*----------------------
+ id='backpack' section 
+ background color tomato
+ ----------------------*/
+const backpackBgColor = document.getElementById('backpack');
+backpackBgColor.style.backgroundColor = 'tomato'
